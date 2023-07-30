bc1d92865369c8adab4e3b5777f778d71545f456
mohankumarpaluru <mohankumarpaluru@gmail.com>
2023-07-30 17:42:16 +0530

    reverted currentDate

--- components/misc/CurrentDate.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CurrentDate = ({ currentDate }) => {
-  return <div>Current date: {currentDate}</div>;
-};
-
-export default CurrentDate;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import CurrentDate from '../components/misc/CurrentDate'; 
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        CurrentDate
         <Main />
         <NextScript />
       </body>
